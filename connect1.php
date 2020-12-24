9d1b4ecfdfaadc6e190f6dbf51409c74078f8fe2
sarath-marvin-7 <sarathheartstoller007@gmail.com>
2020-12-24 21:56:36 +0530

    Changed database

--- connect1.php
+++ connect1.php
@@ -1,20 +1,21 @@
 <?php
-
-$user='epiz_27494345';
-$pass='70jedkyYAdp';
-$db='epiz_27494345_test';
-$db=new mysqli('sql205.epizy.com', $user,$pass,$db) or die("Unable to connect");
-if($db->connect_errno)
-{
-echo $db->connect_error;
-}
-/*
 $user='root';
 $pass='';
 $db='SmartRation';
 $db=new mysqli('localhost', $user,$pass,$db) or die("Unable to connect");
 if($db->connect_errno)
 {   
+echo $db->connect_error;
+}
+
+
+/*
+$user='epiz_27494345';
+$pass='70jedkyYAdp';
+$db='epiz_27494345_test';
+$db=new mysqli('sql205.epizy.com', $user,$pass,$db) or die("Unable to connect");
+if($db->connect_errno)
+{
 echo $db->connect_error;
 }
 */
